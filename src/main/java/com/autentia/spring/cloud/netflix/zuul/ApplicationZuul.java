720a369f6476b389aa9c1ad0307b7d94d5044b94
Jose Mangialomini <jmangialomini@autentia.com>
2017-03-13 09:47:10 +0100

    Zuul server basics

---
+++ src/main/java/com/autentia/spring/cloud/netflix/zuul/ApplicationZuul.java
@@ -0,0 +1,16 @@
+package com.autentia.spring.cloud.netflix.zuul;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+@EnableZuulProxy
+public class ApplicationZuul {
+	
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ApplicationZuul.class).web(true).run(args);
+    }
+}x
